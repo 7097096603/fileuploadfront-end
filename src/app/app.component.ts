7cb1c02ba406f13998f41114b67b2b873fe8431b
jagadeesh <jagadeeshputtupu@gmail.com>
2024-07-07 15:36:27 +0530

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FileUploadDownloadService } from './file-upload-download.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fileupload';
+  selectedFiles : any;
+  currentFile! :File;
+  files: any;
+
+
+  constructor(private http : HttpClient,private fileService : FileUploadDownloadService){}
+  selectFile(event : any){
+this.selectedFiles = event.target.files
+  }
+
+  uploadFile(){
+    if(this.selectedFiles){
+      const file : File = this.selectedFiles.item(0);
+      if(file){
+this.currentFile = file;
+this.fileService.uploadFile(file).subscribe(res=>{
+
+})
+      }
+    }
+  }
+
+  loadFiles() {
+    this.fileService.getFiles().subscribe(
+      (res) => {
+        this.files = res;
+      }
+    );
+  }
+
+  getFileUrl(id: number): string {
+    return `${this.fileService.baseUrl}/files/${id}`;
+  }
 }
---
+++ src/app/file-upload-download.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadDownloadService {
+   baseUrl = 'http://localhost:3000';
+  constructor(private http : HttpClient) { }
+
+  uploadFile(file : File) : Observable<any>{
+    const formData : FormData = new FormData();
+    formData.append('file', file);
+return this.http.post(this.baseUrl + '/upload',formData);
+  }
+
+  getFiles(): Observable<any> {
+    return this.http.get(this.baseUrl +'/files');
+  }
+
+  getFile(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/files/${id}`, { responseType: 'blob' });
+  }
+}
